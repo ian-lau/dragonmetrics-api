37a2ae536b6101e5e2ed4e119a7434652e7d74a8
Ian Lau <30972987+ian-lau@users.noreply.github.com>
2018-04-07 00:29:49 +0800

    Update bulk_create_campaigns.py

--- bulk_create_campaigns.py
+++ bulk_create_campaigns.py
@@ -3,17 +3,14 @@ import base64
 import json
 import csv
 
-username = ''
-password = ''
-credentials = base64.b64encode(bytes(username+':'+password, 'utf-8')).decode('utf-8')
-headers = {
-	'Accept': 'application/json',
-	'Authorization': 'Basic '+credentials,
-	'Content-Type': 'application/json'
-}
-url = 'https://api.dragonmetrics.com/v1.3/campaigns?'
+username = '' # Your API username
+password = '' # Your API password
+campaigns = 'bulk_create_campaigns_template.csv' ### Your new campaigns
 
-reader = csv.DictReader(open('campaigns.csv', 'r'))
+credentials = base64.b64encode(bytes(username+':'+password, 'utf-8')).decode('utf-8')
+headers = {'Accept': 'application/json','Authorization': 'Basic '+credentials,'Content-Type': 'application/json'}
+url = 'https://api.dragonmetrics.com/v1.3/campaigns'
+reader = csv.DictReader(open(campaigns, 'r'))
 for row in reader:
 	r = requests.post(url, data=json.dumps(row), headers=headers)
 	print(r.json())
